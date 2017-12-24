4d20a93d523cec61db2d3e5cee0a35c76ea95d90
Lily Kuong <lilykuong@gmail.com>
2017-12-24 18:04:34 -0500

    minor fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 //Change Fun Facts to a different fact with a set interval
 
-var currFact = document.getElementById("fun-fact");
-
-var facts = ["In college, I was the president, vice president, \
-             	tresurer, secretary, events coordinator, and a mentor for several \
-             	different organizations.",
+const FACT_CHANGE_INTERVAL = 7000;
+const FACT_ID = "fun-fact";
+const FACTS = ["In college, I was the president, vice president, \
+                tresurer, secretary, events coordinator, and a mentor for several \
+                different organizations.",
              "I love getting involved in the community. Last year, I've \
                 volunteered to give out christmas trees to low income households and organized \
                 a program that encourages high school students to pursue engineering.",
              "Corgis are used to herd cattle and are the smallest dogs used for herding.",
              "My favorite place in New York is at the Brooklyn Bridge Park."];
-var i = 1;
 
-const SEVEN_SEC = 7000;
+var i = 1;
+var currFact = document.getElementById(FACT_ID);
 
 setInterval(() => {
-    if (i > facts.length - 1){
+    if (i > FACTS.length - 1){
         i = 0;
     }
-    currFact.innerHTML = facts[i];
+    currFact.innerHTML = FACTS[i];
     i++;
         
-}, SEVEN_SEC);
+}, FACT_CHANGE_INTERVAL);

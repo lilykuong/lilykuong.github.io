3a1d3afdb05b2107e644d2493170b2dd691f348c
Lily Kuong <lilykuong@gmail.com>
2017-12-24 09:59:25 -0500

    + brian's feedback

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,24 @@
-/**
-	Change Fun Facts to a different fact with a set interval
-	*/
-	var fact1 = "In college, I was the president, vice president, \
-	tresurer, secretary, events coordinator, and a mentor for several \
-	different organizations.";
+//Change Fun Facts to a different fact with a set interval
 
-	var fact2 = "I love getting involved in the community. Last year, I've \
-	volunteered to give out christmas trees to low income households and organized \
-	a program that encourages high school students to pursue engineering.";
+var currFact = document.getElementById("fun-fact");
 
-	var fact3 = "Corgis are used to herd cattle and are the smallest dogs used \
-	for herding.";
+var facts = ["In college, I was the president, vice president, \
+             	tresurer, secretary, events coordinator, and a mentor for several \
+             	different organizations.",
+             "I love getting involved in the community. Last year, I've \
+                volunteered to give out christmas trees to low income households and organized \
+                a program that encourages high school students to pursue engineering.",
+             "Corgis are used to herd cattle and are the smallest dogs used for herding.",
+             "My favorite place in New York is at the Brooklyn Bridge Park."];
+var i = 1;
 
-		var fact4 = "My favorite place in New York is at the Brooklyn Bridge Park."
+const SEVEN_SEC = 7000;
 
-	var currFact = document.getElementById("fun-fact");
-
-	var facts = [fact1, fact2, fact3, fact4];
-	var i = 1;
-
-	setInterval(() => {
-		if (i > facts.length - 1){
-			i = 0;
-		}
-		currFact.innerHTML = facts[i];
-		i++;
-	}, 7000)
-
-/**
-	Change the color of the progess bar for CSS and being a wonderful human
-	*/
-	$(function() { 
-		$("#progress-bar-css").addClass("progress-bar-css");
-		$("#progress-bar-human").addClass("progress-bar-human");
-	});
-
+setInterval(() => {
+    if (i > facts.length - 1){
+        i = 0;
+    }
+    currFact.innerHTML = facts[i];
+    i++;
+        
+}, SEVEN_SEC);
